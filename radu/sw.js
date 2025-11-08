331f5c76192889d69e18e6383d65a6ac1b4ddca8
JustIust <156524006+JustIust@users.noreply.github.com>
2025-11-08 08:58:43 +0200

    Delete radu directory

--- radu/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { self.clients.claim(); });
-
-self.addEventListener('message', event => {
-  const data = event.data || {};
-  if(data.type === 'show-notif'){
-    self.registration.showNotification(data.title || 'Reminder', { body: data.body || '' });
-  }
-});
